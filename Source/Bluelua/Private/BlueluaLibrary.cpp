f9905e9e422b473d479b5adaf5b429e25cae84ce
Jash <newzeadev@gmail.com>
2019-03-08 21:52:06 +0800

    BlueluaLibrary::Delay

--- Source/Bluelua/Private/BlueluaLibrary.cpp
+++ Source/Bluelua/Private/BlueluaLibrary.cpp
@@ -1,8 +1,14 @@
 #include "BlueluaLibrary.h"
 
+#include "DelayAction.h"
 #include "Engine/Engine.h"
 #include "Engine/GameViewportClient.h"
+#include "Engine/LatentActionManager.h"
 #include "Engine/World.h"
+#include "LatentActions.h"
+#include "Misc/Guid.h"
+
+#include "LuaFunctionDelegate.h"
 
 UObject* UBlueluaLibrary::GetWorldContext()
 {
@@ -12,4 +18,27 @@ UObject* UBlueluaLibrary::GetWorldContext()
 	}
 
 	return nullptr;
+}
+
+int32 UBlueluaLibrary::Delay(UObject* WorldContextObject, float Duration, int32 InDelegateId, class ULuaFunctionDelegate* InDelegate)
+{
+	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		const int32 DelegateUUID = InDelegateId >= 0 ? InDelegateId : GetTypeHash(FGuid::NewGuid());
+
+		FLatentActionManager& LatentActionManager = World->GetLatentActionManager();
+		if (LatentActionManager.FindExistingAction<FDelayAction>(InDelegate, DelegateUUID) == NULL)
+		{
+			FDelayAction* DelayAction = new FDelayAction(Duration, FLatentActionInfo());
+			DelayAction->ExecutionFunction = ULuaFunctionDelegate::DelegateFunctionName;
+			DelayAction->OutputLink = 0;
+			DelayAction->CallbackTarget = InDelegate;
+
+			LatentActionManager.AddNewAction(InDelegate, DelegateUUID, DelayAction);
+		}
+
+		return DelegateUUID;
+	}
+
+	return -1;
 }
--- Source/Bluelua/Public/BlueluaLibrary.h
+++ Source/Bluelua/Public/BlueluaLibrary.h
@@ -12,4 +12,15 @@ class BLUELUA_API UBlueluaLibrary : public UBlueprintFunctionLibrary
 public:
 	UFUNCTION(BlueprintPure, Category = "Utilities|BlueluaLibrary")
 	static UObject* GetWorldContext();
+
+	/**
+	* Perform a lua function delegate with a delay (specified in seconds).  Calling again while it is counting down will be ignored.
+	*
+	* @param WorldContext	World context.
+	* @param Duration 		length of delay (in seconds).
+	* @param InDelegateId 		DelegateId.
+	* @param InDelegate 	The lua function delegate.
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Utilities|BlueluaLibrary", meta = (WorldContext = "WorldContextObject", Duration = "0.2"))
+	static int32 Delay(UObject* WorldContextObject, float Duration, int32 InDelegateId, class ULuaFunctionDelegate* InDelegate);
 };
